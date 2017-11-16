27c5c1e31b39fe433d27c3f9059a64033c0a1df4
Shibaeva Maria <Bolzuka@gmail.com>
2017-11-16 12:34:46 +0300

    Update and rename Answer.txt to Answer.js

---
+++ Answer.js
@@ -0,0 +1,4 @@
+       const f = function(t, e){e.target.textContent = this+t};   
+       document.querySelector('button').addEventListener('click', f.bind('Нажата первая кнопка', this));  
+       document.querySelector('button:nth-child(2)').addEventListener('click', f.bind('Нажата вторая кнопка', this));  
+       document.querySelector('h1').insertAdjacentHTML('afterend', '<h4 id="author" title="GossJS">Maria Shibaeva</h4>');
